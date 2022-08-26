e3906cce895c317c711b7a3fc98d0c94ebba0f66
unknown <Kana.MATSUMOTO@jpd.co.jp>
2022-08-26 11:18:22 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,43 @@
 // クラスコンポーネントがコメントアウト→Component誰も使ってないため消してよい
 import React from "react";
 
+
+//関数コンポーネント
+const App = () => {
+  //map解説　→繰り返し処理で無駄を省く
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 15},
+    {name: "NoName"}
+  ]
+  
+  return (
+    <div>
+      {
+        profiles.map((profiles, index) => {
+          return <User name={profiles.name} age={profiles.age} />
+        })
+      }
+    </div>
+  )
+
+  //props解説
+  // return (
+  //   <div>
+  //     {/* Userというコンポーネントに対してnameという属性を与える.「nameという属性を与えてUserというコンポーネント呼び出す」 */}
+  //     <User name={"Taro"} age={10} />
+  //     {/* コンポーネントは再利用可能 */}
+  //     <User name={"Hanako"} age={5} />
+
+  //   </div>
+  // )
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name} and {props.age} years old!</div>
+}
+
+
 // class App extends Component {
 //   //AppというクラスがComponentを継承している→Appはクラスコンポーネント
 //   render(){
@@ -35,21 +72,9 @@ import React from "react";
 //   }
 // }
 
-//関数コンポーネント
-const App = ()=>{
-  return (
-    <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-  
-    </div>
-  )
-}
-
-const Cat =()=>{
-  return <div>Meow!</div>
+//デフォルトprops
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
